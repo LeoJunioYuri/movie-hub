1b056bfd8aae7a2eca58df0e919f62ae06c580a9
leojunioyuri <leojunioyuri@hotmail.com>
2024-08-29 23:04:51 -0300

    feat: componente botão de submit

---
+++ src/components/SubmitButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="btn" @click="$emit('click')">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.btn {
+  padding: 0.5rem 1rem;
+  border: none;
+  background-color: #007bff;
+  color: white;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background-color: #0056b3;
+}
+</style>
